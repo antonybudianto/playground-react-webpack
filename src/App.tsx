a75375d497468c63daf06e158015e1414b84ef38
Antony Budianto <antonybudianto@gmail.com>
2023-01-14 09:22:33 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { useEffect } from "react";
 
 const App = () => {
   useEffect(() => {
-    // @ts-ignore: to test swc plugin!
+    // @ts-ignore: to test swc plugin, it should be transformed into `false` bool!
     console.log("hydrate ok", __DEV__);
   }, []);
+
   return <div>R18 Streaming with TS</div>;
 };
 
